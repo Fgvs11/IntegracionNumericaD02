db08e8af97dedf172a431542764dcae23592e7ee
Fgvs-PC <fgvizcainos@gmail.com>
2023-04-23 11:50:49 -0600

    Documentacion

--- Integracion.java
+++ Integracion.java
@@ -1,35 +1,66 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/**
+  *
+  * <h1>Clase Integracion</h1>
+  * <p> Esta es la clase donde todo el proceso interno se ejecuta, <br>
+  * ya que aqui estan presentes los metodos de integracion,<br>
+  * ademas de toda la entrada de datos. <br><br>
+  * </p>
+  * @author Fernando Genaro Vizcaino Sanches / Alejandro Estrada Ponce
+  * @version Ver 1.22474487139
+  *
+  */
 public class Integracion {
+    /** El objeto necesario para la entrada de datos */
     Scanner leer = new Scanner(System.in);
+    /** Codigo para que el mensaje este en color verde */
     public static final String ANSI_GREEN = "\u001B[32m";
+    /** Codigo para que el mensaje este en color amarillo */
     public static final String ANSI_YELLOW = "\u001B[33m";
+    /** Codigo para que el mensaje este en color rojo */
     public static final String ANSI_REED = "\u001B[31m";
+    /** Codigo para que el mensaje este en color predefinido */
     public static final String ANSI_RESET = "\u001B[0m";
-    private float incremento, inicial;
+    /** Almacena el incremento entre valores de x */
+    private float incremento;
+    /** Este es el valor inicial de x */
+    private float inicial;
+    /** Todos los valores que toma x almacenados en un arreglo */
     private double[] filaX;
+    /** Todos los valores que toma y almacenados en un arreglo */
     private double[] filaY;
 
+    /** Es el constructor de la clase, el cual inicializa los arreglos
+     * @param cantidad Cantidad de datos a guardar
+     */
     public Integracion(int cantidad) {
         filaX = new double[cantidad];
         filaY = new double[cantidad];
     }
 
+    /** Metodo que lleva a cabo todo el proceso de ingreso de datos,<br>
+     * llevando a cabo los metodos necesarios
+     * 
+     */
     public void ingresoDatos() {
         incremento = validarIncremento();
         inicial = validarInicial();
         llenarTablaX(incremento, inicial);
         llenarTablaY();
     }
-
+    /**
+     * Llena todo el arreglo de filaX con los valores respectivos de x
+     * @param incre Es el incremento entre cada valor
+     * @param ini Es el valor de la primera x
+     */
     public void llenarTablaX(float incre, float ini) {
         filaX[0] = ini;
         for (int i = 1; i < filaX.length; i++) {
             filaX[i] = filaX[i - 1] + incre;
         }
     }
-
+    /**Llena todo el arreglo de filaX con los valores respectivos de y */
     public void llenarTablaY() {
         System.out.print(ANSI_YELLOW + "AVISO!!! LOS DATOS CON MAS DE 2 DECIMALES SE MOSTRARAN REDONDEADOS\n" + ANSI_RESET);
         System.out.println("Ingresa los datos de [Y]...");
@@ -46,7 +77,9 @@ public class Integracion {
             }
         }
     }
-
+    /** Valida quer el incremento si sea un número positivo
+     * @return Regresa el valor del incremento
+     */
     public float validarIncremento () {
         do {
         try {
@@ -63,7 +96,10 @@ public class Integracion {
         }
     }while (true);
     }
-
+    /**
+     * Verifica que el valor inicial ingresado si sea un numero
+     * @return regresa el valor inicial
+     */
     public float validarInicial () {
         do {
             try {
@@ -75,7 +111,9 @@ public class Integracion {
             }
         }while (true);
     }
-
+    /**
+     * Imprime en formato de tabla los valores obtenidos
+     */
     public void imprimirTabla() {
         System.out.println();
         System.out.printf("+-----------------------------------+%n");
@@ -88,6 +126,10 @@ public class Integracion {
         }
         System.out.printf("+-----------+-----------+-----------+%n");
     }
+    /**
+     * Contiene toda la logica atras de la separacion de intervalos para la aplicacion de las formulas, recordando que un numero arriba de 3 siempre puede ser descompuestos en la suma de multiplos de 3 y de 2
+     * @return regresa el resultado de la integral total
+     */
     public double IntegracionNumerica(){
         if (filaY.length == 2){
             return Trapecios(0, filaY.length -1);
@@ -102,14 +144,19 @@ public class Integracion {
         double ans1 = 0;
         double ans2 = 0;
         if(x != 0){
-            ans1 = Simsom38(0, lim);
+            ans1 = Simpson38(0, lim);
         }
         if(lim != n){
-            ans2 = Simsom13(lim, n);
+            ans2 = Simpson13(lim, n);
         }
         return ans1 + ans2;
     }
-
+    /**
+     * Es la formula de trapecios, no recomendable usarse, por lo cual solo se usa en caso de ingresar solo 2 numeros
+     * @param i Es en que parte del arreglo inicia a tomar en cuenta
+     * @param f Es en que parte del arreglo detenerse
+     * @return  Regresa el resultado de la integral en el intervalo que se especifico con i y f
+     */
     public double Trapecios(int i, int f){
         double ans = 0;
         for(int j = 1; j < f-i;j++ ){
@@ -120,8 +167,13 @@ public class Integracion {
         ans = (incremento/2) * ans;
         return ans;
     }
-
-    public double Simsom13(int i, int f){
+    /**
+     * Es la formula de simpson 1/3, esta solo es aplicable cuando n %2 ==0
+     * @param i Es en que parte del arreglo inicia a tomar en cuenta
+     * @param f Es en que parte del arreglo detenerse
+     * @return  Regresa el resultado de la integral en el intervalo que se especifico con i y f
+     */
+    public double Simpson13(int i, int f){
         double ans = 0;
         for(int j = 1; j < f - i;j++ ){
             if(j%2 == 0){
@@ -134,8 +186,13 @@ public class Integracion {
         ans *= incremento/3;
         return ans;
     }
-
-    public double Simsom38(int i, int f){
+    /**
+     * Es la formula de simpson 3/8, esta solo es aplicable cuando n %3 ==0
+     * @param i Es en que parte del arreglo inicia a tomar en cuenta
+     * @param f Es en que parte del arreglo detenerse
+     * @return  Regresa el resultado de la integral en el intervalo que se especifico con i y f
+     */
+    public double Simpson38(int i, int f){
         double ans = 0;
         for(int j = 1; j < f - i;j++ ){
             if(j%3 == 0){
--- PruebaIntegracion.java
+++ PruebaIntegracion.java
@@ -1,14 +1,35 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/**
+  *
+  * <h1>Clase Integracion</h1>
+  * <p> Esta es la clase donde todo el proceso externo se ejecuta, <br>
+  * ya que aqui estan presentes los metodos de validacion,<br>
+  * ademas del metodo main. <br><br>
+  * </p>
+  * @author Fernando Genaro Vizcaino Sanches / Alejandro Estrada Ponce
+  * @version Ver 1.22474487139
+  *
+  */
 public class PruebaIntegracion {
+	/** Ingresa la cantidad de datos a ingresar */
 	public static int cant;
+	/** El objeto necesario para la entrada de datos */
 	public static Scanner leer = new Scanner(System.in);
+	/** Almacena el incremento entre valores de x */
 	public static final String ANSI_RESET = "\u001B[0m";
+	/** Codigo para que el mensaje este en color amarillo */
 	public static final String ANSI_YELLOW = "\u001B[33m";
+	/** Codigo para que el mensaje este en color rojo */
 	public static final String ANSI_REED = "\u001B[31m";
+	/** Codigo para que el mensaje este en color verde */
 	public static final String ANSI_GREEN = "\u001B[32m";
-
+	/**
+    * Metodo principal de la clase <b>PruebaIntegracion</b>.
+    * <br>
+    * @param args recibe informacion al momento de que se ejecuta la clase, es de tipo <i>String</i>
+    *
+    */
 	public static void main(String args[]) {
 
 		Integracion inte = new Integracion(validarCant());
@@ -17,7 +38,10 @@ public class PruebaIntegracion {
 		double ans = inte.IntegracionNumerica();
 		System.out.printf(ANSI_GREEN + "El resultado es: %.4f" +ANSI_RESET,ans);
 	}
-
+	/**
+	 * Valida que el numero ingresado en cantidad coincida en ser un numero entero positivo 
+	 * @return regresa el numero que va en cantidad
+	 */
 	public static int validarCant() {
 		do {
 			try {
